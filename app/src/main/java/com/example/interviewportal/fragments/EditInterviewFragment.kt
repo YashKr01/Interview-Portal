d64ce7314f69cf0eabe32b37d5b04ddd0179947d
YashKr01 <yashkumar201301@gmail.com>
2022-03-26 12:12:26 +0530

    -> #BUG FIX : Validating fields & lists

--- app/src/main/java/com/example/interviewportal/fragments/EditInterviewFragment.kt
+++ app/src/main/java/com/example/interviewportal/fragments/EditInterviewFragment.kt
@@ -1,7 +1,6 @@
 package com.example.interviewportal.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -56,6 +55,9 @@ class EditInterviewFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val interview = argument.interview
+
+        startTimeInt = interview.startTimeInt
+        endTimeInt = interview.endTimeInt
 
         binding.apply {
             textInterviewTitle.setText(interview.title)
